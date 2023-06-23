d256d9206bf5df5c59f629e53286fb05d1acc3cd
Gabriel Vogel <gabriel.vogel@online.de>
2023-06-24 00:05:04 +0200

    neovim: Fine-tune plugin config for rust and LaTeX

--- nvim/.config/nvim/lua/plugins/latex.lua
+++ nvim/.config/nvim/lua/plugins/latex.lua
@@ -1,10 +1,15 @@
 return {
     {
         "lervag/vimtex",
-        ft = "tex",
+        --ft = "tex",
+        --cmd = "VimtexInverseSearch",
         config = function()
             vim.g.vimtex_view_general_viewer = "okular"
             vim.g.vimtex_view_general_options = "--unique file:@pdf#src:@line@tex"
+            vim.g.vimtex_compiler_latexmk = {
+                -- Put auxiliary files into 'latexmk' subdirectories
+                aux_dir = 'latexmk',
+            }
         end,
     },
 }
